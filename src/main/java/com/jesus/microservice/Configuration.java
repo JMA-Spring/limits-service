c60b16d67723706a8501313bb6ed7fd93968f876
Jesus Monroy <jesus.monroyn@ibm.com>
2018-04-24 12:18:30 -0500

    Se agrego la configuracion para que jale los valores desde el servidor de configuracion,el arhcivo de configuracion del servidor debe tener el mismo nombre que se especifica en el name del bootstrap

--- src/main/java/com/jesus/microservice/Configuration.java
+++ src/main/java/com/jesus/microservice/Configuration.java
@@ -4,20 +4,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limits-service")
+@ConfigurationProperties("limits-service")	// se pone el basename de las variables que se van a usar que estan en el application.properties
 public class Configuration {
 	private int minimum;
 	private int maximum;
-	
+
 	public int getMinimum() {
 		return minimum;
 	}
+
 	public void setMinimum(int minimum) {
 		this.minimum = minimum;
 	}
+
 	public int getMaximum() {
 		return maximum;
 	}
+
 	public void setMaximum(int maximum) {
 		this.maximum = maximum;
 	}
